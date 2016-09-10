d9da0d4aef0cc49506e1d0b13834e7b8f4efda1d
tern456 <kevin.li.4@stonybrook.edu>
2016-09-10 15:26:34 -0400

    Add files via upload

--- Event.java
+++ Event.java
@@ -56,11 +56,34 @@ public class Event {
         return date;
     }
 
+    public void setDate(Date date){
+        this.date = date;
+    }
+
     public int getStart(){
         return start;
     }
 
+    public void setStart(int start){
+        this.start = start;
+    }
+
     public int getEnd(){
         return end;
+    }
+
+    public void setEnd(int end){
+        this.end = end;
+    }
+
+    public Event clone(Event event){
+        Event temp = new Event();
+        temp.setName(event.getName());
+        temp.setHost(event.getHost());
+        temp.setDescription(event.getDescription());
+        temp.setDate(event.getDate());
+        temp.setStart(event.getStart());
+        temp.setEnd(event.getEnd());
+        return temp;
     }
 }
